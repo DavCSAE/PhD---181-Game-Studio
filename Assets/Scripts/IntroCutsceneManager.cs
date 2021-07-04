a26effb0e862c6cff17d114b141fcb71c6f7b381
David <85209872+DavCSAE@users.noreply.github.com>
2021-07-04 21:35:18 +1000

    Started intro cutscenes

---
+++ Assets/Scripts/IntroCutsceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class IntroCutsceneManager : MonoBehaviour
+{
+    [SerializeField] PlayableDirector startCutscene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
